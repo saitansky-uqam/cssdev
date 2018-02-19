752a85ed46a283026d5a79d401fbb0726c72e748
Nelson Moller <moller_clara.nelson@uqam.ca>
2018-02-19 10:16:21 -0500

    MM-822: Ajouter un traitement pour rendre les images inline.

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,8 +1,18 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var cssInlineImages = require('gulp-css-inline-images');
 
 gulp.task('sass2css', function() {
     gulp.src('./sass/main.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('inline-images', function () {
+    gulp.src('css/main.css')
+        .pipe(cssInlineImages({
+            webRoot: '.',
+            path: '/assets/images'
+        }))
+        .pipe(gulp.dest('./build'));
 });
